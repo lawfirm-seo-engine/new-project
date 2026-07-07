ffa0a69386d983e2a866664b0122f9def9d6aa2a
이병권 <cantury77@gmail.com>
2026-07-07 22:07:05 +0900

    Bump OG image cache version

--- functions/_seo.js
+++ functions/_seo.js
@@ -4,7 +4,7 @@ export const RECENT_SITEMAP_DAYS = 14;
 export const RECENT_SITEMAP_LIMIT = 300;
 export const RSS_LIMIT = 120;
 export const SEO_STABILIZED_AT = "2026-06-14";
-export const OG_IMAGE_VERSION = "20260707a1";
+export const OG_IMAGE_VERSION = "20260707a2";
 export const OG_IMAGE_WIDTH = 1254;
 export const OG_IMAGE_HEIGHT = 1254;
 
